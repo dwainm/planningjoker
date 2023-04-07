84c36d510af03104e0860e1a3893adfe51004368
Dwain Maralack <dwain.maralack@gmail.com>
2023-04-07 17:42:06 +0200

    Add the ability to pull in the projects and clean up the file.

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
-	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
@@ -12,32 +12,15 @@ import (
 	"golang.org/x/oauth2"
 )
 
+var client *githubv4.Client
+
 func main() {
 	src := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: getGithubToken()},
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
 
-	client := githubv4.NewClient(httpClient)
-
-	// query{
-    // organization(login: "ORGANIZATION"){
-    //   projectV2(number: NUMBER) {
-    //     id
-    //   }
-    // }
-  // }'
-	var query struct {
-		Viewer struct{
-			Login string
-			WebsiteURL string
-		}
-	}
-
-	err := client.Query(context.Background(), &query, nil)
-	if err != nil {
-		log.Println(err)
-	}
+	client = githubv4.NewClient(httpClient)
 
 	// Views engine
 	engine := html.New("./views", ".html")
@@ -49,7 +32,8 @@ func main() {
 	app.Get("/", func (c *fiber.Ctx) error {
 		// Render index - start with views directory
         return c.Render("index", fiber.Map{
-            "Title": fmt.Sprintf("Hello, %s!", query.Viewer.Login),
+            "Title": fmt.Sprintf(" Met structs! Dag se, %s!", "USER"),
+			"Projects": getProjects(),
         })
 	})
 
@@ -58,4 +42,29 @@ func main() {
 
 func getGithubToken() string {
 	return os.Getenv("PLANNING_JOKER_GH_TOKEN")
+}
+
+func getProjects() []struct { Id string; Title string} {
+		var projectQuery struct {
+		User struct {
+			ProjectsV2 struct{
+				Nodes [] struct{
+					Id string
+					Title string
+				}
+			}  `graphql:"projectsV2(first: 20)"`
+		}  `graphql:"user(login: \"dwainm\")"`
+	}
+
+	// variables := map[string]interface{}{
+	// 	"owner": githubv4.String(owner),
+	// 	"name":  githubv4.String(name),
+	// }
+
+	err := client.Query(context.Background(), &projectQuery, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return projectQuery.User.ProjectsV2.Nodes
 }
